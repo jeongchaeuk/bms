70a959f18a6cd7015c87feb588367666a3951553
jeongchaeuk <jeongchaeuk@gmail.com>
2020-10-30 16:26:49 +0900

    Update board_dummy.sql

--- doc/board_dummy.sql
+++ doc/board_dummy.sql
@@ -1,4 +1,4 @@
-insert into board(id, title, content, hit, img, created_at, created_by)
+insert into board(brd_id, brd_title, brd_content, brd_hit, brd_img, reg_date, reg_userid)
     select 
         board_seq.nextval, 
         dbms_random.string('a', 10), 
@@ -7,5 +7,7 @@ insert into board(id, title, content, hit, img, created_at, created_by)
         null,
         trunc(SYSDATE + DBMS_RANDOM.value(0,366)), 
         dbms_random.string('a', 5) from dual
-connect by level < 99;
+connect by level < 45;
+
+commit;
 
